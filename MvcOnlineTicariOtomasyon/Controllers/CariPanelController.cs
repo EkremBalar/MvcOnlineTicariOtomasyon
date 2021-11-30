496630521c25d632b094cac5a69523c3c151f7f8
ekrembalar09@hotmail.com <ekrembalar09@hotmail.com>
2021-11-30 23:10:04 +0300

    Admin Login işlemi ,düzenlemeler yapıldı.

--- MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/CariPanelController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcOnlineTicariOtomasyon.Models.Siniflar;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,10 +11,28 @@ namespace MvcOnlineTicariOtomasyon.Controllers
     {
         // GET: CariPanel
 
+
+        Context c = new Context();
         [Authorize]
         public ActionResult Index()
         {
+
+            var mail = (string)Session["CariMail"];
+            var degerler = c.Carilers.FirstOrDefault(x => x.CariMail == mail);
+
+            ViewBag.m = mail;
+            return View(degerler);
+        }
+
+        public ActionResult Siparislerim()
+        {
+
+            var mail = (string)Session["CariMail"];
+            var id = c.Carilers.Where(x => x.CariMail == mail.ToString()).Select(y => y.CariId).FirstOrDefault();
+            var degerler = c.SatisHarekets.Where(x => x.Cariid == id).ToList();
             return View();
         }
+
+
     }
 }
