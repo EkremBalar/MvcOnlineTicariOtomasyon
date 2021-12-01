18bb3e15aa640e48909200a95bbc2d04c8b506cf
ekrembalar09@hotmail.com <ekrembalar09@hotmail.com>
2021-12-01 03:59:19 +0300

    Personel resimleri File Upload ile güncellendi. KargoDetay sınıfı oluşturuldu.

---
+++ MvcOnlineTicariOtomasyon/Models/Siniflar/KargoTakip.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class KargoTakip
+    {
+        [Key]
+        public int KargoTakipId { get; set; }
+
+        [Column(TypeName ="Varchar")]
+        [StringLength(10)]
+        public string TakipKodu { get; set; }
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(50)]
+        public string Aciklama { get; set; }
+        public DateTime Tarih { get; set; }
+
+
+
+    }
+}
